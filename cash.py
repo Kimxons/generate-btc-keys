3b00cecd91840856ce3f9c0a1d55ccbdc59cd8fa
kimxons <kitongameshack9@gmail.com>
2020-06-23 18:25:00 -0400

    make it better

--- cash.py
+++ cash.py
@@ -1,4 +1,5 @@
-import os, binascii, hashlib, base58, ecdsa
+import os, binascii
+import hashlib, base58, ecdsa
 from hashlib import sha256
 
 # priv_key = os.urandom(32)
@@ -9,5 +10,5 @@ fullkey = '80' + binascii.hexlify(os.urandom(32)).decode()
 sha256a = hashlib.sha256(binascii.unhexlify(fullkey)).hexdigest()
 sha256a = sha256(binascii.unhexlify(fullkey)).hexdigest()
 sha256b = sha256(binascii.unhexlify(sha256a)).hexdigest()
-WIF = base58.b58decode(binascii.unhexlify(fullkey+sha256b[:8]))
+WIF = base58.b58decode(binascii.unhexlify(fullkey+sha256b[:4]))
 print(fullkey)
